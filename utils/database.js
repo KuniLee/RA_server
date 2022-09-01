7484ed68ec12724f7d74a6fc12da7fe59583b706
Sitnikov <selnetys@gmail.ru>
2022-09-01 16:17:30 +0500

    fix: return to mysql

--- utils/database.js
+++ utils/database.js
@@ -1,28 +1,12 @@
 const { Sequelize } = require('sequelize')
-const DB_NAME = 'racloud-data'
-const USER_NAME = 'ra-server'
-const PASSWORD = 'Yuio9876'
+const DB_NAME = 'ra-server'
+const USER_NAME = 'mysql'
+const PASSWORD = 'oHGIy703yg38hg3'
 require('dotenv').config()
 
-const sequelize = new Sequelize(DB_NAME, null, null, {
-  dialect: 'mssql',
+const sequelize = new Sequelize(DB_NAME, USER_NAME, PASSWORD, {
   host: process.env.DB_IP,
-  dialectOptions: {
-    authentication: {
-      type: 'default',
-      options: {
-        domain: 'WORKGROUP',
-        userName: USER_NAME,
-        password: PASSWORD
-      }
-    },
-    options: {
-      cryptoCredentialsDetails: {
-        minVersion: 'TLSv1'
-      }
-    }
-  }
+  dialect: 'mysql',
 })
-
 
 module.exports = sequelize
